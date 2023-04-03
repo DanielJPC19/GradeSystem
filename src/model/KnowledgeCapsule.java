a169968ccb44ff815f972153e02fc897c532f76b
DanielJPC19 <danieljpc19@gmail.com>
2023-04-03 08:39:39 -0500

    upload KnowledgeCapsule class

---
+++ src/model/KnowledgeCapsule.java
@@ -0,0 +1,50 @@
+package model;
+
+public class KnowledgeCapsule{
+	
+	/**
+	*Name of the collaborator (who register the capsule)
+	*/
+	private String collaboratorName;
+	/**
+	*Capsule id
+	*/
+	private String id;
+	/**
+	*Description, the knowledge or the pharagrap
+	*/
+	private String description;
+	/**
+	*Capsule type
+	*/
+	private String type;
+	/**
+	*The learning of the phase
+	*/
+	private String learning;
+	/**
+	*Approve of the capsule
+	*/
+	private boolean approve;
+	/**
+	*url of the capsule
+	*/
+	private String url;
+	/**
+	*Size of the array Hashtag
+	*/
+	public final static int SIZE_HASHTAG = 3;
+	/**
+	*Important words, hashtag, filters.
+	*/
+	private String[] hashtag;
+	
+	
+	public KnowledgeCapsule(String collaboratorName, String id, String description, String type, String learning, boolean approve, String url, String[] hashtag){
+		this.collaboratorName = collaboratorName;
+		this.id = id;
+		this.description = description;
+		this.type = type;
+		this.learning = type;
+	}
+}
